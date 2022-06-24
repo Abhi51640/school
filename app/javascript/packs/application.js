7bc8be7721451ae3128edbaf8bdcc13a1705b232
Abhishek Pal <abhishek@techwinners.in>
2022-06-24 12:38:14 +0530

    CSS

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+[label ~/rails-bootstrap/app/javascript/packs/application.js]
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import "bootstrap"
+import "../stylesheets/application"
